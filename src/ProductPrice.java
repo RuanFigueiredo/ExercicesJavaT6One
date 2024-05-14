04ea76796dc81f1f84f0a8c67c0759c920dd1a6b
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 16:13:00 -0300

    multiply price and quantity of products

---
+++ src/ProductPrice.java
@@ -0,0 +1,8 @@
+public class ProductPrice {
+    public static void main(String[] args) {
+        double productPrice = 8;
+        int amount = 5;
+
+        System.out.println(productPrice*amount);
+    }
+}
