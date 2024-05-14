32e21785cc883724a61d476f2108a5e529962ce9
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 15:38:22 -0300

    average exercise

---
+++ src/AverageDecimalNotes.java
@@ -0,0 +1,9 @@
+public class AverageDecimalNotes {
+    public static void main(String[] args) {
+        double averageOne = 9.5;
+        double averageTwo = 8.2;
+
+        double finalAverage = (averageOne + averageTwo)/2;
+        System.out.println(finalAverage);
+    }
+}
