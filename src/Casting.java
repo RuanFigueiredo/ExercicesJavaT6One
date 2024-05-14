f096f3ca409f9e4f82c41bff86c569434bc54718
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 15:46:43 -0300

    converting double for int using casting

---
+++ src/Casting.java
@@ -0,0 +1,10 @@
+public class Casting {
+    public static void main(String[] args) {
+       double variableDouble = 8.88;
+       int variableInt = 7;
+
+       double sum = (int)variableDouble + variableInt;
+
+        System.out.println(sum);
+    }
+}
