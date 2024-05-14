bae3d62f6fc9c45ae8a1bd3d1975b3bbb618c485
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 15:59:33 -0300

    Concatenate variables type char and String

---
+++ src/ConcatenateVariables.java
@@ -0,0 +1,9 @@
+public class ConcatenateVariables {
+    public static void main(String[] args) {
+        char letter = 'F';
+        String word = "ord";
+
+        System.out.println(letter + word);
+
+    }
+}
