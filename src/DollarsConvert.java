2dd06f3f37cc53475ba980f7cfb86acbe35cb8f3
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 16:23:10 -0300

    converting dollar for real

---
+++ src/DollarsConvert.java
@@ -0,0 +1,9 @@
+public class DollarsConvert {
+    public static void main(String[] args) {
+        double valueInDollars = 2;
+
+        double convertReal =  valueInDollars * 4.94;
+
+        System.out.println("Value Dollar converted for real is: " + convertReal);
+    }
+}
