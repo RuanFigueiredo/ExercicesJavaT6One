937f2bf0ac82900e0c2ad40fb2ce3a758a7e63ce
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-14 16:30:28 -0300

    Discount price

---
+++ src/Discount.java
@@ -0,0 +1,9 @@
+public class Discount {
+    public static void main(String[] args) {
+        double originalPrice = 10;
+
+        double discountPercentage = originalPrice * 0.90;
+
+        System.out.println(discountPercentage);
+    }
+}
